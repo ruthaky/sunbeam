4b19f8601a91b54947bd787297e4b8759ffba7d7
Ruth Asmamaw <asmamawruth@gmail.com>
2024-12-23 21:55:20 +0300

    feat: contact page

--- app/[lang]/contact/sections/index.tsx
+++ app/[lang]/contact/sections/index.tsx
@@ -6,11 +6,7 @@ import { FiMapPin } from "react-icons/fi";
 import { BsTelephone } from "react-icons/bs";
 import { IoMailOpenOutline } from "react-icons/io5";
 
-export default function ContactSection({
-  contactText,
-}: {
-  contactText: any;
-}) {
+export default function ContactSection({ contactText }: { contactText: any }) {
   const initialFormData = {
     name: "",
     email: "",
@@ -58,9 +54,9 @@ export default function ContactSection({
     }
   };
   return (
-    <div className="flex flex-col lg:flex-row h-auto lg:h-screen  w-full bg-secondary ">
+    <div>
       <div
-        className="w-full flex relative text-white h-screen text-9xl py-100 bg-cover"
+        className="w-full flex relative text-white h-[500px] text-9xl py-100 lg:py-0 bg-cover"
         style={{ backgroundImage: `url(${coffee.src})` }}
       >
         <div className="absolute flex flex-col px-4 lg:px-28 gap-5 bg-secondary bg-opacity-80 top-0 left-0 h-full w-full justify-center">
@@ -73,109 +69,111 @@ export default function ContactSection({
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-6 w-full lg:w-1/3 h-full lg:pt-0 pt-[50px] bg-white px-5 lg:pl-28 lg:pr-12 py-10 lg:py-28">
-        <div className="flex flex-col gap-2 text-[30px] lg:text-[50px]  font-normal tracking-tight leading-none ">
-          {contactText.contactdetail}
-        </div>
-        {/* <div className="text-[25px]  tracking-tight leading-none">
+      <div className="flex flex-col lg:flex-row h-auto lg:h-screen  w-full bg-secondary ">
+        <div className="flex flex-col gap-6 w-full lg:w-1/3 h-full pt-[50px] bg-primary px-5 lg:pl-28 lg:pr-12 py-10 lg:py-28">
+          <div className="flex flex-col gap-2 text-[30px] lg:text-[50px] font-normal tracking-tight leading-none ">
+            {contactText.contactdetail}
+          </div>
+          {/* <div className="text-[25px]  tracking-tight leading-none">
             Whether it's working with rice farmers in
           </div> */}
-        <div className="flex flex-col gap-5">
-          <div className="flex flex-row gap-4 text-[20px] items-center">
-            <div>
-              <FiMapPin size={35} />
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-row gap-4 text-[20px] items-center">
+              <div>
+                <FiMapPin size={35} />
+              </div>
+              <p>{contactText.location}</p>
             </div>
-            <p>{contactText.location}</p>
-          </div>
-          <div className="flex flex-row gap-4 text-[20px] items-center">
-            <BsTelephone size={35} />
-            <p>+1 (234) 56789, +1 987 654 3210</p>
-          </div>
-          <div className="flex flex-row gap-4 text-[20px] items-center">
-            <IoMailOpenOutline size={35} />
-            <p>support@surge.com</p>
+            <div className="flex flex-row gap-4 text-[20px] items-center">
+              <BsTelephone size={35} />
+              <p>+1 (234) 56789, +1 987 654 3210</p>
+            </div>
+            <div className="flex flex-row gap-4 text-[20px] items-center">
+              <IoMailOpenOutline size={35} />
+              <p>support@surge.com</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-6 w-full lg:w-2/3 h-full px-5 lg:px-12 lg:pr-28 py-10 lg:py-28">
-        <div className="flex flex-col gap-2 text-[30px] lg:text-[50px] text-white font-normal tracking-tight leading-none ">
-          {contactText.getintouch}
-        </div>
+        <div className="flex flex-col gap-6 w-full lg:w-2/3 h-full px-5 lg:px-12 lg:pr-28 py-10 lg:py-28">
+          <div className="flex flex-col gap-2 text-[30px] lg:text-[50px] text-white font-normal tracking-tight leading-none ">
+            {contactText.getintouch}
+          </div>
 
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-4 w-full lg:w-[700px]"
-        >
-          <div>
-            <label
-              htmlFor="name"
-              className="text-[20px] font-light block text-white mb-2"
-            >
-              {contactText.name}
-            </label>
-            <input
-              type="text"
-              id="name"
-              value={formData.name}
-              onChange={handleInputChange}
-              className="w-full border-b border-white bg-transparent text-white py-2 h-[30px] lg:h-[30px] focus:outline-none focus:border-b-2 focus:border-white"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="text-[20px] font-light block text-white mb-2"
-            >
-              {contactText.email}
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              className="w-full border-b border-white bg-transparent text-white py-2 h-[30px] lg:h-[30px] focus:outline-none focus:border-b-2 focus:border-white"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="phonenumber"
-              className="text-[20px] font-light block text-white mb-2"
-            >
-              {contactText.phonenumber}
-            </label>
-            <input
-              type="text"
-              id="phonenumber"
-              value={formData.phonenumber}
-              onChange={handleInputChange}
-              className="w-full border-b border-white bg-transparent text-white py-2 h-[30px] lg:h-[30px] focus:outline-none focus:border-b-2 focus:border-white"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="message"
-              className="text-[20px] font-light block text-white mb-2"
-            >
-              {contactText.message}
-            </label>
-            <textarea
-              id="message"
-              value={formData.message}
-              onChange={handleInputChange}
-              className="w-full border-b border-white bg-transparent text-white py-2 lg:h-[60px] focus:outline-none focus:border-b-2 focus:border-white"
-              required
-            />
-          </div>
-          <div className="w-full flex h-auto justify-end">
-            <Button type="submit" variant="contact">
-              {contactText.button}
-            </Button>
-          </div>
-        </form>
-        {status && <p className="text-white mt-4">{status}</p>}
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-4 w-full lg:w-[700px]"
+          >
+            <div>
+              <label
+                htmlFor="name"
+                className="text-[20px] font-light block text-white mb-2"
+              >
+                {contactText.name}
+              </label>
+              <input
+                type="text"
+                id="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                className="w-full border-b border-white bg-transparent text-white py-2 h-[30px] lg:h-[30px] focus:outline-none focus:border-b-2 focus:border-white"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="text-[20px] font-light block text-white mb-2"
+              >
+                {contactText.email}
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                className="w-full border-b border-white bg-transparent text-white py-2 h-[30px] lg:h-[30px] focus:outline-none focus:border-b-2 focus:border-white"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="phonenumber"
+                className="text-[20px] font-light block text-white mb-2"
+              >
+                {contactText.phonenumber}
+              </label>
+              <input
+                type="text"
+                id="phonenumber"
+                value={formData.phonenumber}
+                onChange={handleInputChange}
+                className="w-full border-b border-white bg-transparent text-white py-2 h-[30px] lg:h-[30px] focus:outline-none focus:border-b-2 focus:border-white"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="message"
+                className="text-[20px] font-light block text-white mb-2"
+              >
+                {contactText.message}
+              </label>
+              <textarea
+                id="message"
+                value={formData.message}
+                onChange={handleInputChange}
+                className="w-full border-b border-white bg-transparent text-white py-2 lg:h-[60px] focus:outline-none focus:border-b-2 focus:border-white"
+                required
+              />
+            </div>
+            <div className="w-full flex h-auto justify-end">
+              <Button type="submit" variant="contact">
+                {contactText.button}
+              </Button>
+            </div>
+          </form>
+          {status && <p className="text-white mt-4">{status}</p>}
+        </div>
       </div>
     </div>
   );
