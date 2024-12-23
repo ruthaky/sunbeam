54020021bb3e3416b368a4ce2df656acd5e7dd14
Eyosias5 <eyosiassamson5@gmail.com>
2024-12-23 12:33:56 +0300

    fix: make langanuge consistent and force static pages

--- app/[lang]/products/page.tsx
+++ app/[lang]/products/page.tsx
@@ -1,5 +1,6 @@
+export const dynamic = "force-static";
 import React from "react";
-import AboutSection from "./sections/page";
+import AboutSection from "./sections";
 import { getDictionary } from "../dictionaries";
 
 export default async function About({ params: { lang } }: any) {
