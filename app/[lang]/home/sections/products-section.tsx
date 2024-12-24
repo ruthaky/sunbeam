b292104405c2daa0eb6c9ae4d6f2b5651e304909
Ruth Asmamaw <asmamawruth@gmail.com>
2024-12-24 11:35:22 +0300

    fix: product page responsiveness

--- app/[lang]/home/sections/products-section.tsx
+++ app/[lang]/home/sections/products-section.tsx
@@ -42,24 +42,24 @@ export default function ProductsSection({
   ];
 
   return (
-    <div className="flex flex-col gap-6 lg:gap-10 w-full p-5 py-[50px] lg:py-0 lg:px-28 h-auto lg:h-screen bg-secondary justify-center ">
+    <div className="flex flex-col gap-6 lg:gap-10 w-full p-5 py-[15%] lg:py-0  lg:px-28 h-auto lg:h-screen bg-secondary justify-center ">
       <div className="flex flex-col lg:gap-2 text-[35px] lg:text-[60px] text-white font-semibold tracking-tight leading-none ">
         {productTexts.heading}
         <div className="h-[5px] w-[150px] lg:w-[300px] bg-primary "></div>
       </div>
-      <div className="flex flex-col lg:flex-row w-full h-auto justify-around gap-6">
+      <div className="flex flex-col lg:flex-row w-full h-auto justify-around gap-6 ">
         {products.map((product, index) => (
           <div
             key={index}
-            className="relative cursor-pointer w-full lg:w-1/4 h-full group"
+            className="relative cursor-pointer w-full lg:w-1/4 h-[400px] lg:h-full group"
           >
             <Image
               src={product.image}
               alt={product.title}
-              className="w-full  h-[350px] lg:h-[380px] object-cover"
+              className="w-full  h-full lg:h-[380px] object-cover"
             />
             {/* Hover Overlay */}
-            <div className="absolute inset-0 bg-white/90 flex flex-col items-start py-10 px-4 opacity-75 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-300">
+            <div className="absolute h-auto w-full inset-0 bg-white/90 flex flex-col items-start py-10 px-4 opacity-75 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-300">
               <div className="text-[25px] font-bold">{product.title}</div>
               <div className="text-[17px] mt-2 text-left">
                 {product.description}
