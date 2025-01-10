0050b2e2f264118caaa4be73b3c0835178e91486
Eyosias5 <eyosiassamson5@gmail.com>
2025-01-10 17:42:08 +0300

    fix: skip sitemap in middleware

--- middleware.ts
+++ middleware.ts
@@ -38,7 +38,8 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap).*)",
     // Optional: only run on root (/) URL
     // '/'
   ],
