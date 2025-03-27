7a5c87febe21ab0e503c085a0f62298309fc108f
Ruth Asmamaw <asmamawruth@gmail.com>
2025-03-27 10:44:36 +0300

    grad

--- app/(pages)/graduatestudents/page.tsx
+++ app/(pages)/graduatestudents/page.tsx
@@ -25,7 +25,7 @@ function page() {
 
   return (
     <div className="flex flex-col h-auto  w-full pt-[90px] lg:py-0 items-center bg-[#EFFFEA] justify-center">
-      <div className="flex flex-col lg:flex-row h-auto lg:h-screen w-full pt-[90px] lg:py-0 px-4 lg:px-28 items-center bg-[#EFFFEA] justify-between">
+      <div className="flex flex-col lg:flex-row h-auto lg:h-screen w-full py-[100px] lg:pt-[90px] lg:py-0 px-4 lg:px-28 items-center bg-[#EFFFEA] justify-between">
         <motion.div
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
