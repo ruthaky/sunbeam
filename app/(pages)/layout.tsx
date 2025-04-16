8c79d77ed7af85872c669ccb1bb23448140fe530
Ruth Asmamaw <asmamawruth@gmail.com>
2025-04-16 19:06:25 +0300

    goggle analytics

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -9,7 +9,6 @@ import { Open_Sans } from "next/font/google";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import Script from "next/script";
-import Head from "next/head";
 
 const concert_one = Concert_One({
   weight: ["400", "400"],
@@ -67,63 +66,64 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dk4gxgooc/image/upload/v1744266606/sb11_h21hmb.png"
-        />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta
-          property="og:image:alt"
-          content="Sunbeam- Compassionate ABA therapy in Portland, Oregon. SunBeam ABA Therapy empowers children with autism to learn, grow, and thrive through personalized care and family-centered support."
-        />
-        <meta
-          property="og:image:url"
-          content="https://res.cloudinary.com/dk4gxgooc/image/upload/v1744266606/sb11_h21hmb.png"
-        />
-        <meta
-          property="og:image:secure_url"
-          content="https://res.cloudinary.com/dk4gxgooc/image/upload/v1744266606/sb11_h21hmb.png"
-        />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta
-          property="og:image:alt"
-          content="Sunbeam- Compassionate ABA therapy in Portland, Oregon. SunBeam ABA Therapy empowers children with autism to learn, grow, and thrive through personalized care and family-centered support."
-        />
-        <meta
-          property="og:image:url"
-          content="https://res.cloudinary.com/dk4gxgooc/image/upload/v1744266606/sb11_h21hmb.png"
-        />
-       
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-RC06JS7XMR`}
-          strategy="afterInteractive"
-        >
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/dk4gxgooc/image/upload/v1744266606/sb11_h21hmb.png"
+      />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta
+        property="og:image:alt"
+        content="Sunbeam- Compassionate ABA therapy in Portland, Oregon. SunBeam ABA Therapy empowers children with autism to learn, grow, and thrive through personalized care and family-centered support."
+      />
+      <meta
+        property="og:image:url"
+        content="https://res.cloudinary.com/dk4gxgooc/image/upload/v1744266606/sb11_h21hmb.png"
+      />
+      <meta
+        property="og:image:secure_url"
+        content="https://res.cloudinary.com/dk4gxgooc/image/upload/v1744266606/sb11_h21hmb.png"
+      />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta
+        property="og:image:alt"
+        content="Sunbeam- Compassionate ABA therapy in Portland, Oregon. SunBeam ABA Therapy empowers children with autism to learn, grow, and thrive through personalized care and family-centered support."
+      />
+      <meta
+        property="og:image:url"
+        content="https://res.cloudinary.com/dk4gxgooc/image/upload/v1744266606/sb11_h21hmb.png"
+      />
+      {/* <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "po18vi2fr8");
+  `}
+      </Script> */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-RC06JS7XMR`}
+        strategy="afterInteractive"
+      >
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'G-RC06JS7XMR');
  `}
-          </Script>
         </Script>
-      </Head>
+      </Script>
+     
+
       <body
         className={`${openSans.variable} font-openSans antialiased bg-white  min-h-screen flex flex-col overflow-x-hidden`}
       >
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
+        {" "}
         <MantineProvider>
           <div className="block lg:hidden">
             <MobileNavbar />
