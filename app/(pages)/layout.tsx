7442b84716846bcdfe109f21cc701341df70d81c
Ruth Asmamaw <asmamawruth@gmail.com>
2025-04-17 08:59:19 +0300

    ggg

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -114,6 +114,16 @@ export default function RootLayout({
       <body
         className={`${openSans.variable} font-openSans antialiased bg-white  min-h-screen flex flex-col overflow-x-hidden`}
       >
+          <Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "r4la1u9ahq");
+  `}
+</Script>
+
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-RC06JS7XMR"
           strategy="afterInteractive"
